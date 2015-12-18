34d3a7d5580a80c3c640ee4f4e5e0c3b4c810fc4
Martin Manø <mail@m-mano.dk>
2015-12-18 01:57:07 +0100

    ADD - Comments to core .cs  file

--- mmOAuth.Core/OAuth.cs
+++ mmOAuth.Core/OAuth.cs
@@ -24,38 +24,59 @@ namespace ManoSoftware.mmOAuth.Core
         private readonly OAuthHttpServer _httpServer;
         private Thread _httpServerThread;
 
-        public OAuth(IOAuthProvider provider)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OAuth"/> class.
+        /// </summary>
+        /// <param name="provider">Oauth Provider</param>
+        /// <param name="port">Port for Web server to listen. Must be the port you direct back to!</param>
+        public OAuth(IOAuthProvider provider, int port)
         {
             _provider = provider;
 
-            _httpServer = new OAuthHttpServer(8080, _provider.GetSuccessUri());
+            _httpServer = new OAuthHttpServer(port, _provider.GetSuccessUri());
             _httpServer.OAuthCodeRecived += _httpServer_OAuthCodeRecived;
         }
 
+        /// <summary>
+        /// Fired when a authentication code is recived from the web server
+        /// </summary>
         void _httpServer_OAuthCodeRecived(object sender, StringEventArg args)
         {
             StopOAuthRedirectServer();
-
-            if (OAuthCodeRecived != null)
-                OAuthCodeRecived(sender, args);
+            OAuthCodeRecived?.Invoke(sender, args);
         }
 
+        /// <summary>
+        /// Starts a browser, and redirects user to login page.
+        /// </summary>
         public void GotoAuthorization()
         {
             Process.Start(_provider.GetAuthorizationUri() + _provider.GetAuthorizationParameters());
         }
 
+        /// <summary>
+        /// Start the Web server, to catch the user when returning from the authentication page.
+        /// </summary>
         public void StartOAuthRedirectServer()
         {
             _httpServerThread = new Thread(_httpServer.Listen);
             _httpServerThread.Start();
         }
 
+        /// <summary>
+        /// Stops the Web server
+        /// </summary>
         public void StopOAuthRedirectServer()
         {
             _httpServer.Stop();
         }
 
+        /// <summary>
+        /// Gets the access token.
+        /// </summary>
+        /// <param name="oauthCode">The oauth code.</param>
+        /// <returns>Valid access token.</returns>
+        /// <exception cref="Exception">Exception containing the errormessage if one occoured</exception>
         public async Task<string> GetAccessToken(string oauthCode)
         {
             var client = new HttpClient { BaseAddress = new Uri(_provider.GetAccessTokenUri()) };
@@ -73,6 +94,12 @@ namespace ManoSoftware.mmOAuth.Core
             throw new Exception(_provider.ExtractErrorMessage(message));
         }
 
+        /// <summary>
+        /// Verifies a given oAuth Token
+        /// </summary>
+        /// <param name="oAuthToken">The o authentication token.</param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
         public async Task<TokenInfo> VerifyToken(string oAuthToken)
         {
             string uri = _provider.GetVerificationUri();
